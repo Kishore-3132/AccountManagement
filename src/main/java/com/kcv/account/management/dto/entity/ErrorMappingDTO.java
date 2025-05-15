f2f0e996aa98dda60dace1c765511ba0d9a80104
kishore-3132 <kishoredeenan3132@gmail.com>
2025-05-15 15:32:47 +0530

    Changed CODE as uniqueConstraints in ERROR_MAPPING table

--- src/main/java/com/kcv/account/management/dto/entity/ErrorMappingDTO.java
+++ src/main/java/com/kcv/account/management/dto/entity/ErrorMappingDTO.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Data
 @Entity
-@Table(name = "ERROR_MAPPING")
+@Table(name = "ERROR_MAPPING", uniqueConstraints={@UniqueConstraint(columnNames={"CODE"})})
 public class ErrorMappingDTO {
     @Id
     @GeneratedValue
